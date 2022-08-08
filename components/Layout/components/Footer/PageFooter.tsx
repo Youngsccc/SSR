821e41cfd4243f38b84958a00387932b47c9a55e
尚橙 <walkingsc@163.com>
2022-08-08 08:38:03 +0800

    feat(202208072039): 页脚文案更新

--- components/Layout/components/Footer/PageFooter.tsx
+++ components/Layout/components/Footer/PageFooter.tsx
@@ -1,8 +1,16 @@
 import { NextPage } from 'next';
 import { NextPageProps } from '@/types/common';
+import { prefix } from '@/constant/index';
+import styles from './index.module.scss';
 
 const PageFooter: NextPage<NextPageProps> = () => {
-  return <div>页脚</div>;
+  return (
+    <div className={styles[`${prefix}-footer`]}>
+      <span className={styles[`${prefix}-footer-content`]}>
+        施工单位 ©️orange-web
+      </span>
+    </div>
+  );
 };
 
 export default PageFooter;
